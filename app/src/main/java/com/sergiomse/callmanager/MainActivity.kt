434eb2571edac726558aa163caf82f8c4d6708c6
sergiomse <sergiomse@gmail.com>
2017-03-22 22:20:28 +0100

    Permission management

--- app/src/main/java/com/sergiomse/callmanager/MainActivity.kt
+++ app/src/main/java/com/sergiomse/callmanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sergiomse.callmanager
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -9,11 +10,19 @@ import android.view.Menu
 import android.view.MenuItem
 import com.sergiomse.callmanager.database.Database
 import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest.permission
+import android.Manifest.permission.WRITE_CALENDAR
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+
 
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.simpleName
+    private val PERMISSION_REQUEST_READ_PHONE_STATE = 100;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +32,56 @@ class MainActivity : AppCompatActivity() {
 
         val layoutManager = LinearLayoutManager(this)
         numberRV.layoutManager = layoutManager
+
+        requestPermissionReadPhoneState()
+    }
+
+    private fun requestPermissionReadPhoneState() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+
+                // TODO Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                requestPermissionReadPhoneState()
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.READ_PHONE_STATE),
+                        PERMISSION_REQUEST_READ_PHONE_STATE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    //TODO show alert when the permission is not granted
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_READ_PHONE_STATE -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+        }
     }
 
     override fun onResume() {
