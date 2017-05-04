afd3dc2bd1d8a14ba417dd6676ded75d80a8a6df
sergiomse <sergiomse@gmail.com>
2017-05-04 21:16:18 +0100

    Improved permission management

--- app/src/main/java/com/sergiomse/callmanager/MainActivity.kt
+++ app/src/main/java/com/sergiomse/callmanager/MainActivity.kt
@@ -4,9 +4,11 @@ import android.Manifest
 import android.app.NotificationManager
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
@@ -14,11 +16,14 @@ import android.support.v7.widget.LinearLayoutManager
 import com.sergiomse.callmanager.database.Database
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import android.net.Uri.fromParts
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.simpleName
-    private val PERMISSION_REQUEST_READ_PHONE_STATE = 100;
+    private val PERMISSION_REQUEST_READ_PHONE_STATE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +33,11 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         numberRV.layoutManager = layoutManager
 
-        requestPermissionReadPhoneState()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissionReadPhoneState()
+        }
 
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !notificationManager.isNotificationPolicyAccessGranted) {
             val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
             startActivity(intent)
@@ -43,54 +49,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun requestPermissionReadPhoneState() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_PHONE_STATE)) {
-
-                // TODO Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                requestPermissionReadPhoneState()
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.READ_PHONE_STATE),
-                        PERMISSION_REQUEST_READ_PHONE_STATE);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-    }
-
-    //TODO show alert when the permission is not granted
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            PERMISSION_REQUEST_READ_PHONE_STATE -> {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return
-            }
-        }
-    }
-
     override fun onResume() {
         super.onResume()
 
@@ -99,5 +57,42 @@ class MainActivity : AppCompatActivity() {
         val adapter = NumbersAdapter(this, numberList)
         numberRV.adapter = adapter
         db.cleanup()
+
     }
+
+    private fun requestPermissionReadPhoneState() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_PHONE_STATE),
+                    PERMISSION_REQUEST_READ_PHONE_STATE);
+
+        }
+    }
+
+    //TODO show alert when the permission is not granted
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_READ_PHONE_STATE -> {
+
+                if (!grantResults.isNotEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Snackbar.make(mainLayout,
+                            "Call Manager needs access to the phone state permission in order to detect incoming calls.",
+                            Snackbar.LENGTH_INDEFINITE)
+                            .setAction("Configure", {v ->
+                                val intent = Intent()
+                                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                val uri = Uri.fromParts("package", packageName, null)
+                                intent.data = uri
+                                startActivity(intent)
+                            })
+                            .show()
+
+                }
+                return
+            }
+        }
+    }
+
 }
